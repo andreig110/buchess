e9274c18f84bbbf9802299a7e4b9f8737a57f8a0
andreig110 <42849998+andreig110@users.noreply.github.com>
2019-01-08 23:41:41 +0200

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,30 @@
+/*
+ * Buchess, a UCI chess engine derived from Stockfish 9
+ * Copyright (C) 2018-2019 Andrei Guga
+ * File creation date: 2018-10-01
+ * 
+ * Buchess is free, and distributed under the
+ * GNU General Public License Version 3 (GPLv3).
+ */
+
 #include <iostream>
+
+#include "bitboard.h"
+#include "log.h"
+#include "misc.h"
+#include "uci.h"
 
 int main(int argc, char* argv[])
 {
+    std::cout << engine_info() << std::endl;
+    
+    Bitboards::init();
+    
+    LOG::openFile();
+    LOG::log(argc, argv);
+    
+    UCI::loop();
+    
+    LOG::closeFile();
     return 0;
 }
